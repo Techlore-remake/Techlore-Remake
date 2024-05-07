25a97088503461a924d7995f177f4b8a191e09b2
Vidul <66768720+VidulHB@users.noreply.github.com>
2024-05-07 02:44:28 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,71 @@
 const express = require("express");
 const index = express.Router();
 const chalk = require("chalk");
-const traffic = require('../PrivateModels/traffic');
+const traffic = require("../PrivateModels/traffic");
 
 function log() {
-  console.log(
-    chalk.bgCyanBright.bold(" [Router] Index Successfully Booted ")
-  );
+  console.log(chalk.bgCyanBright.bold(" [Router] Index Successfully Booted "));
 }
 
 setTimeout(log, 1000);
 
 index.use(async (req, res, next) => {
-  let date = new Date()
-  let formateddate = new Date(date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2))
-  let data = await traffic.findOne({Day: formateddate}).exec()
-  if(data){
-   await traffic.findOneAndUpdate({Day: formateddate}, {$set: { Visits: (data.Visits+1)}}).exec()
-  }else{
-   await traffic.create({
+  let date = new Date();
+  let formateddate = new Date(
+    date.getFullYear() +
+      "-" +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("0" + date.getDate()).slice(-2),
+  );
+  let data = await traffic.findOne({ Day: formateddate }).exec();
+  if (data) {
+    await traffic
+      .findOneAndUpdate(
+        { Day: formateddate },
+        { $set: { Visits: data.Visits + 1 } },
+      )
+      .exec();
+  } else {
+    await traffic.create({
       Day: formateddate,
-  Visits: 1,
-  API_Requests: 0,
-  Database_Changes: 0,
-    })
+      Visits: 1,
+      API_Requests: 0,
+      Database_Changes: 0,
+    });
   }
-  next()
-})
+  next();
+});
 
-index.get('/', async (req, res, next) => {
-  const user = req.session.user
-  const session = req.session
-  res.render('index.ejs', {
+index.get("/", async (req, res, next) => {
+  const user = req.session.user;
+  const session = req.session;
+  res.render("index.ejs", {
     user: user,
-    session: session
+    session: session,
+  });
+});
+index.get("/contact", async (req, res, next) => {
+  res.render("contact.ejs");
+});
+
+index.get("/logout", async (req, res, next) => {
+  req.session.destroy(function (err) {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.redirect("/");
+    }
   });
 });
 
-index.get('/logout', async (req, res, next) => {
-  req.session.destroy(function(err) {
-    if (err) {
-      console.log(err);
-      res.send("Error")
-    } else {
-      res.redirect('/')
-    }
-  })
-})
-
-index.get('/login', async (req, res, next) => {
-  const user = req.session.user
+index.get("/login", async (req, res, next) => {
+  const user = req.session.user;
   if (user) {
-    res.redirect('/')
+    res.redirect("/");
   } else {
-    res.render('login.ejs', { user: user });
+    res.render("login.ejs", { user: user });
   }
 });
 
