5250efc0f95b35223345f8189d36475da9cff5a6
Vidul <66768720+VidulHB@users.noreply.github.com>
2024-05-07 05:43:25 +0000

    update #2

--- models/users.js
+++ models/users.js
@@ -2,26 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 (UserSchema = new Schema({
-    id: Number,
     name: String,
     username: String,
     email: String,
-    address: {
-      street: String,
-      suite: String,
-      city: String,
-      zipcode: String,
-      geo: {
-        lat: String,
-        lng: String
-      }
-    },
-    phone: String,
-    website: String,
-    company: {
-      name: String,
-      catchPhrase: String,
-      bs: String
+    password: String,
+    admin: {
+      status: Boolean,
+      level: Number
     }
 })),
   (Users = mongoose.model("users", UserSchema));
---
+++ public/main.js
@@ -0,0 +1,8 @@
+function Navbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
